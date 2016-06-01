fa8129b2690cf7ca479a2dbe2cd491f69bf97b18
Osspial <osspial@gmail.com>
2016-05-31 21:00:42 -0400

    Update `main` example to allow movement of bezier curve control points

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,10 +8,10 @@ extern crate glutin;
 use bev::{Bez3o2d};
 
 use gfx::traits::FactoryExt;
-use gfx::{Device, Primitive};
+use gfx::{Factory, Device, Primitive, BufferRole, Bind, Slice, IndexBuffer};
 use gfx::state::Rasterizer;
 
-use glutin::Event;
+use glutin::{Event, ElementState, MouseButton};
 
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
@@ -28,8 +28,10 @@ gfx_pipeline!{ pipe {
 }}
 
 fn main() {
+    let (mut win_x, mut win_y) = (720, 720);
+
     let builder = glutin::WindowBuilder::new()
-        .with_dimensions(720, 720)
+        .with_dimensions(win_x, win_y)
         .with_multisampling(16)
         .with_title("Hello Bézier".into());
     let (window, mut device, mut factory, main_color, _) =
@@ -50,19 +52,8 @@ fn main() {
          0.5,  0.5
     );
 
-    const SAMPLES: usize = 31;
-    let mut cverts = [Vertex{ pos: [0.0, 0.0], col: [1.0, 1.0, 1.0] }; SAMPLES * 2];
-    for i in 0..SAMPLES {
-        let t = i as f32/(SAMPLES-1) as f32;
-
-        let interp = curve.interp(t);
-
-        let perp = curve.slope(t).normalize().perp() * 0.01;
-        cverts[i*2].pos = (-perp + interp).into();
-        cverts[i*2 + 1].pos = (perp + interp).into();
-    }
-
-    let circle = gen_circle(16, 0.02, [1.0, 0.0, 0.0]);
+    let radius = 0.02;
+    let circle = gen_circle(16, radius, [1.0, 0.0, 0.0]);
     let mut indices = vec![0u16; 42];
     for (ind, i) in indices.iter_mut().enumerate() {
         let ind = ind + 3;
@@ -73,7 +64,18 @@ fn main() {
         }
     }
 
-    let (cvert_buffer, cvert_slice) = factory.create_vertex_buffer_with_slice(&cverts, ());
+    // A vec of curve vertices
+    const SAMPLES: usize = 31;
+    let mut cverts = [Vertex{ pos: [0.0, 0.0], col: [1.0, 1.0, 1.0] }; SAMPLES * 2];
+    let cvert_buffer = factory.create_buffer_dynamic(cverts.len(), BufferRole::Vertex, Bind::empty()).unwrap();
+    let cvert_slice = Slice {
+        start: 0,
+        end: cvert_buffer.len() as u32,
+        base_vertex: 0,
+        instances: None,
+        buffer: IndexBuffer::Auto
+    };
+
     let (cir_buffer, cir_slice) = factory.create_vertex_buffer_with_slice(&circle, &indices[..]);
     let mut data = pipe::Data {
         vbuf: cvert_buffer.clone(),
@@ -81,14 +83,52 @@ fn main() {
         out: main_color
     };
 
+    // The index of the selected control point. If -1, no control point is selected.
+    let mut selected = -1;
+    // Mouse x, mouse y
+    let (mut mox, mut moy) = (0.0, 0.0);
     'main: loop {
         for event in window.poll_events() {
             match event {
                 Event::Closed => break 'main,
+                Event::Resized(x, y) => {win_x = x; win_y = y;}
+                Event::MouseMoved(x, y) => {
+                    mox = pix_to_float(x, win_x); 
+                    moy = -pix_to_float(y, win_y);
+                }
+                Event::MouseInput(ElementState::Pressed, MouseButton::Left) => {
+                    for (i, c) in curve.iter().enumerate() {
+                        let dist = (c.x-mox).hypot(c.y-moy);
+                        if dist <= radius {
+                            selected = i as isize;
+                        }
+                    }
+                }
+                Event::MouseInput(ElementState::Released, MouseButton::Left) => selected = -1,
                 _ => ()
             }
         }
 
+        // If any control point is selected, move that control point with the mouse.
+        if 0 <= selected {
+            let c = &mut curve[selected as usize];
+            c.x = mox;
+            c.y = moy;
+        }
+
+        // Calculate curve vertices based on control point position.
+        for i in 0..SAMPLES {
+            let t = i as f32/(SAMPLES-1) as f32;
+
+            let interp = curve.interp(t);
+
+            let perp = curve.slope(t).normalize().perp() * 0.01;
+            cverts[i*2].pos = (-perp + interp).into();
+            cverts[i*2 + 1].pos = (perp + interp).into();
+        }
+        encoder.update_buffer(&cvert_buffer, &cverts, 0).unwrap(); // Upload calculated vertex positions to vertex buffer
+
+        // Drawing code
         encoder.clear(&data.out, [0.0, 0.0, 0.0, 1.0]);
 
         data.offset = [0.0, 0.0];
@@ -110,6 +150,12 @@ fn main() {
         window.swap_buffers().unwrap();
         device.cleanup();
     }
+}
+
+pub fn pix_to_float(p: i32, s: u32) -> f32 {
+    let s = s as f32;
+    let p = p as f32;
+    2.0 * (p - s/2.0)/s
 }
 
 fn gen_circle(divs: u16, scale: f32, col: [f32; 3]) -> Vec<Vertex> {
