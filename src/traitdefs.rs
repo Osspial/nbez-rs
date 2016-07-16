0e4712beaad91e9670b9327fbe1e235f7f0f4e6d
Osspial <osspial@gmail.com>
2016-07-16 16:35:13 -0400

    Updated PVOps to also require Zero trait, updated Point and Vector to require Clone trait

--- src/traitdefs.rs
+++ src/traitdefs.rs
@@ -1,18 +1,21 @@
 //! Various trait aliases to simplify generics in other modules. Every trait in this module is
 //! implemented for all possible valid types.
-use num_traits;
+use num_traits::float;
+use num_traits::identities::Zero;
+use num_traits::cast::FromPrimitive;
 use std::fmt::Debug;
 use std::ops::{Add, Sub, Mul, Div, Neg};
 
-pub trait Float: num_traits::float::Float + num_traits::cast::FromPrimitive + Debug {}
-impl<F> Float for F where F: num_traits::float::Float + num_traits::cast::FromPrimitive + Debug {}
+pub trait Float: float::Float + FromPrimitive + Debug {}
+impl<F> Float for F where F: float::Float + FromPrimitive + Debug {}
 
 pub trait PVOps<F>:		
 		Add<Self, Output = Self> +
 		Sub<Self, Output = Self> +
 		Mul<F, Output = Self> +
 		Div<F, Output = Self> +
-		Neg<Output = Self>
+		Neg<Output = Self> +
+		Zero
 
 		where Self: Sized,
 			  F: Float {}
@@ -23,11 +26,13 @@ impl<F: Float, PV> PVOps<F> for PV
 			Sub<PV, Output = PV> +
 			Mul<F, Output = PV> +
 			Div<F, Output = PV> +
-			Neg<Output = PV> {}
+			Neg<Output = PV> +
+			Zero {}
 
 pub trait Point<F, V>: 
 		From<V> +
-		Clone + 
+		Clone +
+		Copy + 
 		PVOps<F>
 
 		where Self: Sized,
@@ -37,14 +42,15 @@ pub trait Point<F, V>:
 impl<F, P, V> Point<F, V> for P 
         where F: Float,
         	  V: Vector<F, P>,
-              P: From<V> + Clone + PVOps<F> {}
+              P: From<V> + Clone + Copy + PVOps<F> {}
 
 pub trait Vector<F: Float, P: Point<F, Self>>: 
 		From<P> +
 		Clone +
+		Copy +
 		PVOps<F> {}
 
 impl<F, P, V> Vector<F, P> for V 
         where F: Float, 
-              P: Point<F, V>, 
-              V: From<P> + Clone + PVOps<F> {}
+              P: Point<F, V>,
+              V: From<P> + Clone + Copy + PVOps<F> {}
